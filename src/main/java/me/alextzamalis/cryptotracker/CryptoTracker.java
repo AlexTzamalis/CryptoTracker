86645afa475549251472071f9474a080606095c9
Alex Tzamalis <alextzamalis16@gmail.com>
2026-04-27 01:29:33 +0300

    doc: javadoc included for Main Class, briefly explaining the use-case of this class. as well as stating, Author name, UEL, and repository @link

--- src/main/java/me/alextzamalis/cryptotracker/CryptoTracker.java
+++ src/main/java/me/alextzamalis/cryptotracker/CryptoTracker.java
@@ -3,6 +3,22 @@ package me.alextzamalis.cryptotracker;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * <h1><b> Main Class and Application Entryway. </b></h1>
+ *
+ * <p> Represents a JavaFX application designed to track cryptocurrency information.
+ * This class extends {@link Application} and provides the main structure
+ * for the user interface and application lifecycle. </p>
+ *
+ * <p> All Classes of this project will provide a JavaDoc style of documentation
+ * to provide simple and enhanced information about each class, and possibly methods.
+ * for any viewer reading and analyzing this Open Source Repository.
+ * for additional project related information read @ README.md </p>
+ *
+ * @uel 2872177
+ * @repository github.com/AlexTzamalis/CryptoTracker
+ * @author Alexandros Tzamalis
+ */
 public class CryptoTracker extends Application {
     /**
      * The main entry point for all JavaFX applications.
