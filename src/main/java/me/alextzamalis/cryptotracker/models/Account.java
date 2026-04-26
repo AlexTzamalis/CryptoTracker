097fa0ae0967f363629089cbcca926b309412ea6
Alex Tzamalis <alextzamalis16@gmail.com>
2026-04-27 02:04:49 +0300

    chore: created getters/setters for Account fields

--- src/main/java/me/alextzamalis/cryptotracker/models/Account.java
+++ src/main/java/me/alextzamalis/cryptotracker/models/Account.java
@@ -20,7 +20,7 @@ public abstract class Account {
      * @param role
      */
     protected Account(String username, String passwordHash, String displayName, AccountRole role) {
-
+        this(UUID.randomUUID().toString(), username, passwordHash, displayName, role);
     }
 
     /**
@@ -40,11 +40,74 @@ public abstract class Account {
     }
 
     /**
-     * Supported account roles.
+     * Supported account roles
      */
     public enum AccountRole {
         CLIENT,
         ADMIN
     }
 
+    /**
+     * REturns the unique account identifier
+     * @return account ID
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Returns the username used for login
+     * @return account username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * updates the username parameter
+     * @param username the new username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Returns the stored password hash
+     * @return password hash
+     */
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    /**
+     * Updates the stored password hash
+     * @param passwordHash the new password hasgh
+     */
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash  = passwordHash;
+    }
+
+    /**
+     * Return the user-facing display name
+     * @return display name
+     */
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    /**
+     * Updates the user-facing display name
+     * @param displayName the new display name
+     */
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    /**
+     * Returns the account role
+     * @return account role
+     */
+    public AccountRole getRole() {
+        return role;
+    }
 }
